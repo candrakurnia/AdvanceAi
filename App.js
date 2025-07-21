19e0309baa05ae1ebfadf42c54a64cc929f1b974
rifqi <rifqi.mufdianto@fanintek.com>
2025-07-21 16:01:58 +0700

    implementing liveness

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
+import {SHA256} from 'crypto-js';
 import React from 'react';
 import {
   SafeAreaView,
-  ScrollView,
   StatusBar,
   StyleSheet,
   Text,
   useColorScheme,
   View,
+  Button,
+  NativeModules,
 } from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 const Section = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
@@ -44,39 +38,196 @@ const Section = ({children, title}) => {
 };
 
 const App = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+  const onVerifyLiveness = async () => {
+    NativeModules.LivenessModule.initSDKByLicense('Indonesia', false);
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+    const token = await fetchToken();
+
+    const signatureId = await fetchSignatureId(token);
+
+    if (!token) return;
+
+    const response = await fetchLicense(token);
+
+    const license = response.data.license;
+
+    // NativeModules.LivenessModule.bindUser("your user id")
+    NativeModules.LivenessModule.set3DLivenessTimeoutMills(5000);
+    // NativeModules.LivenessModule.setVideoRecorderConfig(true, 60);
+    NativeModules.LivenessModule.isDetectOcclusion(true);
+
+    if (signatureId) {
+      NativeModules.LivenessModule.setSignatureId(signatureId);
+    }
+
+    NativeModules.LivenessModule.setLicenseAndCheck(
+      license,
+      successCode => {
+        console.log('Success: ' + successCode);
+
+        NativeModules.LivenessModule.startLiveness(
+          successJsonData => {
+            const livenessId = successJsonData.livenessId;
+            const imageBase64 = successJsonData.livenessBase64Str;
+      
+
+            fetchScoring(token, signatureId, livenessId, imageBase64);
+
+            // const payload = {
+            //   livenessId: livenessId,
+            //   signatureId: signatureId,
+            //   resultType: imageBase64,
+            // };
+
+            // fetch(
+            //   'https://api.advance.ai/openapi/liveness/v3/detection-result',
+            //   {
+            //     method: 'POST',
+            //     headers: {
+            //       'X-ACCESS-TOKEN': token,
+            //       'Content-Type': 'application/json',
+            //     },
+            //     body: JSON.stringify(payload),
+            //   },
+            // )
+            //   .then(response => response.json())
+            //   .then(data => {
+            //     console.log('Response Scoring:', data);
+            //   })
+            //   .catch(error => {
+            //     console.error('Fetch Error:', error);
+            //   });
+          },
+          failedJsonData => {
+            console.log('Failed Liveness: ' + JSON.stringify(failedJsonData));
+          },
+        );
+      },
+      errorCode => {
+        console.error('Error: ' + errorCode);
+      },
+    );
+  };
+
+  const fetchLicense = async token => {
+    try {
+      const response = await fetch(
+        'https://api.advance.ai/openapi/liveness/v1/auth-license',
+        {
+          method: 'POST',
+          headers: {
+            'X-ACCESS-TOKEN': token,
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            licenseEffectiveSeconds: 600,
+            applicationId: 'com.myprojectadvance',
+          }),
+        },
+      );
+
+      const data = await response.json();
+      console.log('Response:', data);
+      return data;
+    } catch (error) {
+      console.error('Fetch Error:', error);
+    }
+  };
+
+  const fetchScoring = async (token, signatureId, livenessId, base64image) => {
+    try {
+      const payload = {
+        livenessId: livenessId,
+        // signatureId: signatureId,
+        resultType: base64image,
+      };
+      console.log({payload});
+      console.log({token});
+      const response = await fetch(
+        'https://api.advance.ai/openapi/liveness/v3/detection-result',
+        {
+          method: 'POST',
+          headers: {
+            'X-ACCESS-TOKEN': token,
+            'Content-Type': 'application/json',
+          },
+          body: payload,
+        },
+      );
+      const data = await response.json();
+      console.log('Response Scoring:', data);
+      return data;
+    } catch (error) {
+      console.error('Fetch Error:', error);
+    }
+  };
+
+  const fetchToken = async () => {
+    try {
+      const secretKey = '5f463360c65fbbb7';
+      const accessKey = 'b5008258bdd8a455';
+      const timestamp = (Date.now() + 300).toString();
+
+      console.log({timestamp});
+
+      const rawSignature = accessKey + secretKey + timestamp;
+
+      const signature = SHA256(rawSignature).toString();
+
+      const payload = {
+        accessKey,
+        signature,
+        timestamp,
+        periodSecond: 3600,
+      };
+
+      console.log({payload});
+
+      const response = await fetch(
+        'https://api.advance.ai/openapi/auth/ticket/v1/generate-token',
+        {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(payload),
+        },
+      );
+      const dataRaw = await response.json();
+      console.log('Response Token:', dataRaw.data.token);
+      return dataRaw.data.token;
+    } catch (error) {
+      console.error('Fetch Error:', error);
+      return null;
+    }
+  };
+
+  const fetchSignatureId = async token => {
+    try {
+      const response = await fetch(
+        'https://api.advance.ai/liveness/ext/v1/generate-signature-id',
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'X-ACCESS-TOKEN': token,
+          },
+        },
+      );
+      const dataRaw = await response.json();
+      console.log('Response Signature:', dataRaw.data.signatureId);
+      return dataRaw.data.signatureId;
+    } catch (error) {
+      console.error('Fetch Error:', error);
+      return null;
+    }
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView>
+      <View>
+        <Button title="Verify Liveness" onPress={onVerifyLiveness} />
+      </View>
     </SafeAreaView>
   );
 };
